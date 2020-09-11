7e597fdd2a8aa4eaeeaad76408821c02a508da8e
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-11 16:31:04 -0400

    Write braille conversion to braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,5 @@
+require './lib/braille_converter'
+
 file_names = {
   input: ARGV[0],
   output: ARGV[1]
@@ -6,7 +8,8 @@ file_names = {
 output_file_name = file_names[:output]
 
 input_file_content = File.open(file_names[:input]).read
+braille = BrailleConverter.new(input_file_content).convert
 
-File.open(file_names[:output], 'w') { |file| file.write(input_file_content) }
+File.open(file_names[:output], 'w') { |file| file.write(braille) }
 
-puts "Created '#{output_file_name}'' containing #{input_file_content.length} characters."
+puts "Created '#{output_file_name}' containing #{input_file_content.length} characters."
