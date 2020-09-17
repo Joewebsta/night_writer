6f3f4b660197fb92d9ac8d2b0fee7208748584bf
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-17 19:03:25 -0400

    Braille output begins new line after 80 characters.

--- lib/braille_converter.rb
+++ lib/braille_converter.rb
@@ -10,12 +10,28 @@ class BrailleConverter
   def convert_to_braille
     letters = text.chars
     braille_chars = letters.map { |letter| BrailleChar.new(letter).convert_letter_to_braille }
-    rows_array = [[], "\n", [], "\n", []]
+    rows_array = [[], [], []]
 
     braille_chars.each_with_object(rows_array) do |char, array|
       array[0] << char[0]
-      array[2] << char[1]
-      array[3] << char[2]
-    end.join
+      array[1] << char[1]
+      array[2] << char[2]
+    end
+  end
+
+  def format_braille(braille_row_arr)
+    row1 = braille_row_arr[0].join
+    row2 = braille_row_arr[1].join
+    row3 = braille_row_arr[2].join
+    output = []
+    require 'pry'; binding.pry
+
+    until row1.length.zero?
+      output << row1.slice!(0..79) + "\n"
+      output << row2.slice!(0..79) + "\n"
+      output << row3.slice!(0..79) + "\n"
+    end
+    require 'pry'; binding.pry
+    output.join
   end
 end
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -8,8 +8,9 @@ file_names = {
 output_file_name = file_names[:output]
 
 input_file_content = File.open(file_names[:input]).read
-braille = BrailleConverter.new(input_file_content).convert_to_braille
-
-File.open(file_names[:output], 'w') { |file| file.write(braille) }
+braille_converter = BrailleConverter.new(input_file_content)
+converted_braille = braille_converter.convert_to_braille
+final_braille = braille_converter.format_braille(converted_braille)
+File.open(file_names[:output], 'w') { |file| file.write(final_braille) }
 
 puts "Created '#{output_file_name}' containing #{input_file_content.length} characters."
