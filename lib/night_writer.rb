806f277f02c75f6d083d4a0ce69fb0575d919629
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-18 13:33:04 -0400

    Simplify ARGV variables.

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,16 +1,14 @@
 require './lib/braille_converter'
 
-file_names = {
-  input: ARGV[0],
-  output: ARGV[1]
-}
+file_in = ARGV[0]
+file_out = ARGV[1]
 
-output_file_name = file_names[:output]
+input_file_content = File.read(file_in).chomp
 
-input_file_content = File.open(file_names[:input]).read
 braille_converter = BrailleConverter.new(input_file_content)
 converted_braille = braille_converter.convert_to_braille
 final_braille = braille_converter.format_braille(converted_braille)
-File.open(file_names[:output], 'w') { |file| file.write(final_braille) }
 
-puts "Created '#{output_file_name}' containing #{input_file_content.length} characters."
+File.open(file_out, 'w') { |file| file.write(final_braille) }
+
+puts "Created '#{file_out}' containing #{input_file_content.length} characters."
